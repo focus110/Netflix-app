c499b8a8fd7092aceeaa78e5bdac4552598f7a71
focus110 <iamemmaochuko@gmail.com>
2022-08-23 11:58:20 +0100

    header comp(main.js), get popular movies

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
+import { Main } from "./pages/index";
+
+// bhospajkxqoxqyudpv@nthrl.com
+// 7f0c1a3363b89fde763cc4e6f7cd7e47
 
 function App() {
-  return <div className="App">Hello</div>;
+  return (
+    <div className="">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/main/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../../component/Navbar";
+import Maincomp from "../../component/Main";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <Maincomp />
+    </div>
+  );
+};
+
+export default Main;
