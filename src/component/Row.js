f90ac15d75c8209e13778624035d993b4a553dd4
focus110 <iamemmaochuko@gmail.com>
2022-08-24 10:24:54 +0100

    movie row

---
+++ src/component/Row.js
@@ -0,0 +1,57 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { FaHeart, FaRegHeart } from "react-icons/fa";
+
+const Row = ({ title, fetchURL }) => {
+  const [movies, setMovie] = useState([]);
+  const [like, setLike] = useState(false);
+
+  useEffect(() => {
+    axios.get(fetchURL).then((response) => {
+      setMovie(response.data.results);
+    });
+  }, [fetchURL]);
+
+  return (
+    <>
+      <h2 className="text-white font-bold md:text-xl p-4">{title}</h2>
+      <div className="relative flex items-center">
+        <div id={"slider"}>
+          {movies.map((item, id) => {
+            return (
+              <div
+                key={id}
+                className="w-[160px] sm:w-[200px] md:w-[240px] lg:w-
+              [280px] inline-block cursor relative p-2"
+              >
+                <img
+                  className="w-full h-auto block"
+                  src={`${
+                    !item?.backdrop_path
+                      ? "https://images.unsplash.com/photo-1478720568477-152d9b164e26?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8N3x8bW92aWV8ZW58MHx8MHx8&auto=format&fit=crop&w=400&q=60"
+                      : "https://image.tmdb.org/t/p/w500" + item?.backdrop_path
+                  }`}
+                  alt={item.title}
+                />
+                <div className="absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white">
+                  <p className="white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center">
+                    {item?.title}
+                  </p>
+                  <p>
+                    {like ? (
+                      <FaHeart className="absolute top-4 left-4 text-gray-300" />
+                    ) : (
+                      <FaRegHeart className="absolute top-4 left-4 text-gray-300" />
+                    )}
+                  </p>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Row;
--- src/pages/main/Main.js
+++ src/pages/main/Main.js
@@ -1,12 +1,19 @@
 import React from "react";
 import Navbar from "../../component/Navbar";
 import Maincomp from "../../component/Main";
+import requests from "../../Requests";
+import Row from "../../component/Row";
 
 const Main = () => {
   return (
     <div>
       <Navbar />
       <Maincomp />
+      <Row title="Up UpComing" fetchURL={requests.requestUpcoming} />
+      <Row title="Popular" fetchURL={requests.requestPopular} />
+      <Row title="TopRated" fetchURL={requests.requestTopRated} />
+      <Row title="Trending" fetchURL={requests.requestTrending} />
+      <Row title="Horror" fetchURL={requests.requestHorror} />
     </div>
   );
 };
